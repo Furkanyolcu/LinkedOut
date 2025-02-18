5630cb91dcec085de4467f6b314b6c5823e3f640
muratyildizzz <yildizmurat42953@gmail.com>
2025-02-18 22:17:24 +0300

    model kısmı düzenleme

--- LinkedOut/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace LinkedOut.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
